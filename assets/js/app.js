d46216ebe2ffbe1e33d7e3eb7d5679d76495d897
Nix Siow <nixsiow@hotmail.com>
2015-05-21 17:40:46 +0800

    fix bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,19 +12,19 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
-    templateUrl: '../partials/landing_page.html',
+    templateUrl: 'partials/landing_page.html',
     controller: 'LandingPageController'
   });
   $routeProvider.when('/waitlist', {
-    templateUrl: 'assets/partials/waitlist.html',
+    templateUrl: 'partials/waitlist.html',
     controller: 'WaitlistController'
   });
   $routeProvider.when('/register', {
-    templateUrl: 'assets/partials/register.html',
+    templateUrl: 'partials/register.html',
     controller: 'AuthController'
   });
   $routeProvider.when('/login', {
-    templateUrl: 'assets/partials/login.html',
+    templateUrl: 'partials/login.html',
     controller: 'AuthController'
   });
   $routeProvider.otherwise({redirectTo: '/'});
